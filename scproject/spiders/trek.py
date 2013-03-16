d5e1c0c57133c64297493211b3bf577877716e45
Fikhtengol <lijunchina@foxmail.com>
2013-03-16 21:08:48 +0800

    feather:1.add bloomfilter;bugfix:1.ascii' codec can't encode characters by reload(sys);

--- scproject/spiders/trek.py
+++ scproject/spiders/trek.py
@@ -1,8 +1,12 @@
 from scrapy.spider import BaseSpider
 from scrapy.selector import HtmlXPathSelector
 from scproject.items import TrekItem
-#encoding=utf-8
+from pybloom import ScalableBloomFilter
+import sys
+reload(sys)
+sys.setdefaultencoding( "utf-8" )
 
+sbf = ScalableBloomFilter(initial_capacity=100*100, error_rate=0.0001, mode=ScalableBloomFilter.SMALL_SET_GROWTH)
 class DmozSpider(BaseSpider):
    name = "trek"
    #allowed_domains = ["dmoz.org"]
@@ -17,9 +21,11 @@ class DmozSpider(BaseSpider):
        for url in urls:
            item = TrekItem()
            if url.startswith('http://') or url.startswith('https://'):
-               item['url'] = url+'\n'
-               items.append(item)
-               items.append(self.make_requests_from_url(url))
+               if url not in sbf:
+                  item['url'] = url+'\n'
+                  items.append(item)
+                  items.append(self.make_requests_from_url(url))
+                  sbf.add(url)
                
            #items.append(item)
        return items
